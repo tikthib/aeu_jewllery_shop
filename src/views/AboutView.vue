0373b932d0d95467ba5a691db34a21b0d75ec59b
Soklay LIM <limsoklay01@gmail.com>
2024-05-01 23:27:08 +0700

    test

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,7 +5,8 @@
 <template>
   <main>
     <!-- <TheSlider /> -->
-
+    <h1>Hello!</h1>
+    <p>My  Name Soklay</p>
     <div class="container m-auto">
       <div class="title font-bold text-center text-2xl">
         <h3>About Page</h3>
