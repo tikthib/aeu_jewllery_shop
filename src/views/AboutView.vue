d49ebe366d29989174c9d0dc8dbfc621c9ac3826
Soklay LIM <limsoklay01@gmail.com>
2024-05-03 00:10:01 +0700

    test push

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,9 +5,7 @@
 <template>
   <main>
     <!-- <TheSlider /> -->
-    <h1>Hello!</h1>
-    <p>My  Name Soklay</p>
-    <h1>Hello! 2</h1>
+    <h1>Hello 2!</h1>
     <p>My  Name Soklay 2</p>
     <div class="container m-auto">
       <div class="title font-bold text-center text-2xl">
